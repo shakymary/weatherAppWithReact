4352d521473286aa95f5226b5f47af5378d85689
Shakila Marando <marandoshakila1@gmail.com>
2020-08-28 00:36:24 -0700

    Updates

---
+++ precache-manifest.066fd39bef9dcf6e9e933cb4837949bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d1af4c15cdd7ce12f07fd49debd632",
+    "url": "/weatherAppWithReact/index.html"
+  },
+  {
+    "revision": "31696a0c07ac3d9a6a4a",
+    "url": "/weatherAppWithReact/static/css/main.46e725ea.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weatherAppWithReact/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherAppWithReact/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31696a0c07ac3d9a6a4a",
+    "url": "/weatherAppWithReact/static/js/main.a307bae4.chunk.js"
+  },
+  {
+    "revision": "cd38d7d90da927717644",
+    "url": "/weatherAppWithReact/static/js/runtime-main.2af5f831.js"
+  },
+  {
+    "revision": "97ce2ce738d31a3da2297d802453484b",
+    "url": "/weatherAppWithReact/static/media/cold.97ce2ce7.jpg"
+  },
+  {
+    "revision": "3cfb1beda60470c7407b5c86b37aa3b9",
+    "url": "/weatherAppWithReact/static/media/warm.3cfb1bed.jpg"
+  }
+]);
